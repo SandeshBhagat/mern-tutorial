4cb519b50ecbe0ce1fcee71dce84f00e6dd5e3ff
sandesh bhagat <sandesh bhagat@ho.rnt.ai>
2023-12-29 19:09:05 +0530

    new changes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,26 +1,29 @@
 const asyncHandeler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel')
+
 
 // get goals
-const getGoals = asyncHandeler(async (req, res) => {
-	const goals = await Goal.find();
+const getGoals = asyncHandeler(async(req, res) => {
+	const goals = await Goal.find({user:req.user.id});
 	res.status(200).json(goals);
 });
 
 // set goal
-const setGoal = asyncHandeler(async (req, res) => {
+const setGoal = asyncHandeler(async(req, res) => {
 	if (!req.body.text) {
 		res.status(400);
 		throw new Error('please add a text field');
 	}
 	const goal = await Goal.create({
 		text: req.body.text,
+		user:req.user.id
 	});
 	res.status(200).json(goal);
 });
 
 // update goal
-const updateGoal = asyncHandeler(async (req, res) => {
+const updateGoal = asyncHandeler(async(req, res) => {
 	const goal = await Goal.findById(req.params.id);
 
 	if (!goal) {
@@ -28,6 +31,21 @@ const updateGoal = asyncHandeler(async (req, res) => {
 		throw new Error('Goal Not Found!');
 	}
 
+	const user = await User.findById(req.user.id)
+
+	// check for user
+
+	if(!user){
+		res.status(401)
+		throw new Error('User not found')
+	}
+
+	// check if logged in user matches to goal user
+	if(goal.user.toString() !== user.id){
+		res.status(401)
+		throw new Error('User not authorized')
+
+	}
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	});
@@ -35,12 +53,27 @@ const updateGoal = asyncHandeler(async (req, res) => {
 });
 
 // delete goal
-const deleteGoal = asyncHandeler(async (req, res) => {
+const deleteGoal = asyncHandeler(async(req, res) => {
 	const goal = await Goal.findById(req.params.id);
 
 	if (!goal) {
 		res.status(400);
 		throw new Error('Goal Not Found!');
+	}
+	const user = await User.findById(req.user.id)
+
+	// check for user
+
+	if(!user){
+		res.status(401)
+		throw new Error('User not found')
+	}
+
+	// check if logged in user matches to goal user
+	if(goal.user.toString() !== user.id){
+		res.status(401)
+		throw new Error('User not authorized')
+
 	}
 
 	await Goal.findByIdAndDelete(req.params.id);
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const goalRoutes = require('./routes/goalRoutes');
+const userRoutes = require('./routes/userRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', goalRoutes);
+app.use('/api/users', userRoutes);
 app.use(errorHandler);
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
